9c3992c189f32d2999c3d70332cb13885e31fe1a
unknown <0320161.gcbs@rub.edu.bt>
2024-02-29 14:54:58 +0600

    added new lines

--- session1.py
+++ session1.py
@@ -1,1 +1,7 @@
 print('Hello, World')
+print('Hello, Ngawang')
+# pushing to git hub
+# in the begining
+# 1. git init, 2.git status, 3.git add . and git status
+# 4. git commit -m "first file" 5. git status
+#
