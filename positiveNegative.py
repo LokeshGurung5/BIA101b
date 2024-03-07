746277fedafce38905e70026309d887b12af3479
LokeshGurung5 <141401191+LokeshGurung5@users.noreply.github.com>
2024-03-07 14:23:51 +0600

    Add files via upload

---
+++ positiveNegative.py
@@ -0,0 +1,8 @@
+#1 user input and converting it into string
+a = float(input("Choose one number and determine if it is +ve or -ve: "))
+if a >= 1:
+    print("your number is positive")
+if a < 0:
+    print("your number is negative")
+if a == 0:
+    print("your number is zero")
